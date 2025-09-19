130b0ef208456ae58acd2ecf1802a193adfdfc49
Loe Jubin <joe@not-consensys.net>
2025-09-19 21:43:02 +0000

    Always be lubin

---
+++ test/SomeTokenTest.sol
@@ -0,0 +1,273 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.26;
+
+import {SomeToken} from "contracts/SomeToken.sol";
+import {Test} from "lib/forge-std/src/Test.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+import "forge-std/console.sol";
+
+contract SomeTokenTest is Test {
+    SomeToken token;
+    address owner;
+    address user1;
+    address user2;
+
+    function setUp() public {
+        owner = makeAddr("owner");
+        user1 = makeAddr("user1");
+        user2 = makeAddr("user2");
+
+        // Deploy the token with test name and symbol
+        vm.prank(owner);
+        token = new SomeToken("Test Token", "TEST");
+    }
+
+    function test_initialization() public {
+        // Test token name and symbol
+        assertEq(token.name(), "Test Token");
+        assertEq(token.symbol(), "TEST");
+        assertEq(token.decimals(), 18);
+
+        // Test initial supply
+        uint256 expectedSupply = 1_000_000_000 * 1e18; // 1 billion tokens
+        assertEq(token.totalSupply(), expectedSupply);
+
+        // Test that owner receives all tokens initially
+        assertEq(token.balanceOf(owner), expectedSupply);
+    }
+
+    function test_ownership() public {
+        // Test that ownership is renounced (transferred to address(0))
+        assertEq(token.owner(), address(0));
+
+        // Test that only owner can call owner functions (should fail since owner is address(0))
+        vm.expectRevert();
+        token.transferOwnership(user1);
+    }
+
+    function test_erc20_functionality() public {
+        // Test transfer
+        uint256 transferAmount = 1000 * 1e18;
+        vm.prank(owner);
+        token.transfer(user1, transferAmount);
+
+        assertEq(token.balanceOf(user1), transferAmount);
+        assertEq(token.balanceOf(owner), 1_000_000_000 * 1e18 - transferAmount);
+
+        // Test approval and transferFrom
+        vm.prank(user1);
+        token.approve(user2, transferAmount);
+
+        assertEq(token.allowance(user1, user2), transferAmount);
+
+        vm.prank(user2);
+        token.transferFrom(user1, user2, transferAmount);
+
+        assertEq(token.balanceOf(user2), transferAmount);
+        assertEq(token.balanceOf(user1), 0);
+        assertEq(token.allowance(user1, user2), 0);
+    }
+
+    function test_transfer_insufficient_balance() public {
+        uint256 transferAmount = 1_000_000_001 * 1e18; // More than total supply
+
+        vm.prank(owner);
+        vm.expectRevert();
+        token.transfer(user1, transferAmount);
+    }
+
+    function test_transferFrom_insufficient_allowance() public {
+        uint256 transferAmount = 1000 * 1e18;
+
+        // First transfer some tokens to user1
+        vm.prank(owner);
+        token.transfer(user1, transferAmount);
+
+        // Try to transferFrom without approval
+        vm.prank(user2);
+        vm.expectRevert();
+        token.transferFrom(user1, user2, transferAmount);
+    }
+
+    function test_transferFrom_insufficient_balance() public {
+        uint256 transferAmount = 1000 * 1e18;
+
+        // First transfer some tokens to user1
+        vm.prank(owner);
+        token.transfer(user1, transferAmount);
+
+        // Approve user2 to spend user1's tokens
+        vm.prank(user1);
+        token.approve(user2, transferAmount);
+
+        // Transfer all tokens from user1 to user2
+        vm.prank(user2);
+        token.transferFrom(user1, user2, transferAmount);
+
+        // Try to transfer more than user1 has (should fail)
+        vm.prank(user2);
+        vm.expectRevert();
+        token.transferFrom(user1, user2, 1);
+    }
+
+    function test_approve() public {
+        uint256 approveAmount = 1000 * 1e18;
+
+        vm.prank(owner);
+        token.approve(user1, approveAmount);
+
+        assertEq(token.allowance(owner, user1), approveAmount);
+    }
+
+    function test_increaseAllowance() public {
+        uint256 initialAmount = 500 * 1e18;
+        uint256 increaseAmount = 300 * 1e18;
+
+        vm.prank(owner);
+        token.approve(user1, initialAmount);
+
+        vm.prank(owner);
+        token.increaseAllowance(user1, increaseAmount);
+
+        assertEq(token.allowance(owner, user1), initialAmount + increaseAmount);
+    }
+
+    function test_decreaseAllowance() public {
+        uint256 initialAmount = 1000 * 1e18;
+        uint256 decreaseAmount = 300 * 1e18;
+
+        vm.prank(owner);
+        token.approve(user1, initialAmount);
+
+        vm.prank(owner);
+        token.decreaseAllowance(user1, decreaseAmount);
+
+        assertEq(token.allowance(owner, user1), initialAmount - decreaseAmount);
+    }
+
+    function test_decreaseAllowance_underflow() public {
+        uint256 initialAmount = 1000 * 1e18;
+        uint256 decreaseAmount = 1001 * 1e18; // More than allowance
+
+        vm.prank(owner);
+        token.approve(user1, initialAmount);
+
+        vm.prank(owner);
+        vm.expectRevert();
+        token.decreaseAllowance(user1, decreaseAmount);
+    }
+
+    function test_transfer_to_zero_address() public {
+        uint256 transferAmount = 1000 * 1e18;
+
+        vm.prank(owner);
+        vm.expectRevert();
+        token.transfer(address(0), transferAmount);
+    }
+
+    function test_transferFrom_to_zero_address() public {
+        uint256 transferAmount = 1000 * 1e18;
+
+        // First transfer some tokens to user1
+        vm.prank(owner);
+        token.transfer(user1, transferAmount);
+
+        // Approve user2 to spend user1's tokens
+        vm.prank(user1);
+        token.approve(user2, transferAmount);
+
+        // Try to transfer to zero address
+        vm.prank(user2);
+        vm.expectRevert();
+        token.transferFrom(user1, address(0), transferAmount);
+    }
+
+    function test_approve_zero_address() public {
+        uint256 approveAmount = 1000 * 1e18;
+
+        vm.prank(owner);
+        vm.expectRevert();
+        token.approve(address(0), approveAmount);
+    }
+
+    function test_totalSupply_immutable() public {
+        uint256 initialSupply = token.totalSupply();
+
+        // Transfer some tokens
+        vm.prank(owner);
+        token.transfer(user1, 1000 * 1e18);
+
+        // Total supply should remain the same
+        assertEq(token.totalSupply(), initialSupply);
+    }
+
+    function test_balanceOf() public {
+        uint256 initialBalance = token.balanceOf(owner);
+        assertEq(initialBalance, 1_000_000_000 * 1e18);
+
+        // Check balance of address with no tokens
+        assertEq(token.balanceOf(user1), 0);
+    }
+
+    function test_events() public {
+        uint256 transferAmount = 1000 * 1e18;
+
+        // Test Transfer event
+        vm.expectEmit(true, true, true, true);
+        emit IERC20.Transfer(owner, user1, transferAmount);
+
+        vm.prank(owner);
+        token.transfer(user1, transferAmount);
+
+        // Test Approval event
+        uint256 approveAmount = 500 * 1e18;
+        vm.expectEmit(true, true, true, true);
+        emit IERC20.Approval(owner, user1, approveAmount);
+
+        vm.prank(owner);
+        token.approve(user1, approveAmount);
+    }
+
+    function test_renounceOwnership() public {
+        // Since ownership is already renounced in constructor,
+        // we can't test renounceOwnership directly
+        // But we can verify that owner is address(0)
+        assertEq(token.owner(), address(0));
+    }
+
+    function test_constructor_parameters() public {
+        // Test with different name and symbol
+        SomeToken customToken = new SomeToken("Custom Token", "CUSTOM");
+
+        assertEq(customToken.name(), "Custom Token");
+        assertEq(customToken.symbol(), "CUSTOM");
+        assertEq(customToken.decimals(), 18);
+        assertEq(customToken.totalSupply(), 1_000_000_000 * 1e18);
+    }
+
+    function test_fuzz_transfer(uint256 amount) public {
+        // Bound the amount to reasonable values
+        amount = bound(amount, 1, token.balanceOf(owner));
+
+        uint256 initialBalanceOwner = token.balanceOf(owner);
+        uint256 initialBalanceUser1 = token.balanceOf(user1);
+
+        vm.prank(owner);
+        token.transfer(user1, amount);
+
+        assertEq(token.balanceOf(owner), initialBalanceOwner - amount);
+        assertEq(token.balanceOf(user1), initialBalanceUser1 + amount);
+    }
+
+    function test_fuzz_approve(uint256 amount) public {
+        // Bound the amount to reasonable values
+        amount = bound(amount, 0, 1_000_000_000 * 1e18);
+
+        vm.prank(owner);
+        token.approve(user1, amount);
+
+        assertEq(token.allowance(owner, user1), amount);
+    }
+}
---
+++ test/TokenLaunchpadTest.sol
@@ -0,0 +1,478 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.26;
+
+import {TokenLaunchpad} from "contracts/launchpad/TokenLaunchpad.sol";
+import {SomeToken} from "contracts/SomeToken.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {ITokenLaunchpad} from "contracts/interfaces/ITokenLaunchpad.sol";
+import {ICLMMAdapter} from "contracts/interfaces/ICLMMAdapter.sol";
+import {Test} from "lib/forge-std/src/Test.sol";
+import {MockERC20} from "contracts/mocks/MockERC20.sol";
+
+import "forge-std/console.sol";
+
+/// @title MockCLMMAdapter
+/// @notice Mock implementation of ICLMMAdapter for testing
+contract MockCLMMAdapter is ICLMMAdapter {
+    address public launchpad;
+    mapping(address => uint256) public claimedFees0;
+    mapping(address => uint256) public claimedFees1;
+
+    // Mock pool addresses for different tokens
+    mapping(address => address) public tokenToPool;
+
+    // Mock swap amounts
+    uint256 public mockSwapAmountOut = 1000 * 1e18;
+    uint256 public mockSwapAmountIn = 1000 * 1e18;
+
+    // Mock fee amounts
+    uint256 public mockFee0 = 100 * 1e18;
+    uint256 public mockFee1 = 200 * 1e18;
+
+    constructor() {
+        launchpad = msg.sender;
+    }
+
+    function addSingleSidedLiquidity(AddLiquidityParams memory _params) external override returns (address pool) {
+        // Mock pool address based on token addresses
+        pool = address(uint160(uint256(keccak256(abi.encodePacked(_params.tokenBase, _params.tokenQuote, block.timestamp)))));
+        tokenToPool[address(_params.tokenBase)] = pool;
+        return pool;
+    }
+
+    function swapWithExactOutput(IERC20 _tokenIn, IERC20 _tokenOut, uint256 _amountOut, uint256 _maxAmountIn)
+        external override returns (uint256 amountIn) {
+        // Transfer tokens from caller
+        _tokenIn.transferFrom(msg.sender, address(this), _maxAmountIn);
+        // Transfer output tokens to caller
+        _tokenOut.transfer(msg.sender, _amountOut);
+        return _maxAmountIn;
+    }
+
+    function swapWithExactInput(IERC20 _tokenIn, IERC20 _tokenOut, uint256 _amountIn, uint256 _minAmountOut)
+        external override returns (uint256 amountOut) {
+        // Transfer input tokens from caller
+        _tokenIn.transferFrom(msg.sender, address(this), _amountIn);
+        // Transfer output tokens to caller
+        _tokenOut.transfer(msg.sender, mockSwapAmountOut);
+        return mockSwapAmountOut;
+    }
+
+    function claimFees(address _token) external override returns (uint256 fee0, uint256 fee1) {
+        fee0 = mockFee0;
+        fee1 = mockFee1;
+        claimedFees0[_token] += fee0;
+        claimedFees1[_token] += fee1;
+
+        // Transfer mock fees to caller
+        IERC20(_token).transfer(msg.sender, fee0);
+        return (fee0, fee1);
+    }
+
+    function claimedFees(address _token) external view override returns (uint256 fee0, uint256 fee1) {
+        return (claimedFees0[_token], claimedFees1[_token]);
+    }
+
+    // Helper functions for testing
+    function setMockSwapAmountOut(uint256 _amount) external {
+        mockSwapAmountOut = _amount;
+    }
+
+    function setMockSwapAmountIn(uint256 _amount) external {
+        mockSwapAmountIn = _amount;
+    }
+
+    function setMockFees(uint256 _fee0, uint256 _fee1) external {
+        mockFee0 = _fee0;
+        mockFee1 = _fee1;
+    }
+}
+
+/// @title TestableTokenLaunchpad
+/// @notice Concrete implementation of TokenLaunchpad for testing
+contract TestableTokenLaunchpad is TokenLaunchpad {
+    function _distributeFees(address _token0, address _owner, address _token1, uint256 _amount0, uint256 _amount1)
+        internal override {
+        // Simple distribution: 50% to owner, 50% to treasury
+        address treasury = address(0x1234567890123456789012345678901234567890);
+
+        IERC20(_token0).transfer(_owner, _amount0 / 2);
+        IERC20(_token0).transfer(treasury, _amount0 / 2);
+
+        IERC20(_token1).transfer(_owner, _amount1 / 2);
+        IERC20(_token1).transfer(treasury, _amount1 / 2);
+    }
+}
+
+contract TokenLaunchpadTest is Test {
+    TestableTokenLaunchpad launchpad;
+    MockCLMMAdapter adapter;
+    MockERC20 fundingToken;
+
+    address owner;
+    address cron;
+    address user1;
+    address user2;
+    address treasury;
+
+    function setUp() public {
+        owner = makeAddr("owner");
+        cron = makeAddr("cron");
+        user1 = makeAddr("user1");
+        user2 = makeAddr("user2");
+        treasury = makeAddr("treasury");
+
+        // Deploy mock contracts
+        fundingToken = new MockERC20("Funding Token", "FUND", 18);
+        adapter = new MockCLMMAdapter();
+        launchpad = new TestableTokenLaunchpad();
+
+        // Initialize the launchpad
+        launchpad.initialize(owner, address(fundingToken), address(adapter));
+
+        // Set up cron
+        vm.prank(owner);
+        launchpad.setCron(cron);
+
+        // Set launch ticks
+        vm.prank(cron);
+        launchpad.setLaunchTicks(-206_200, -180_000, 886_000);
+
+        // Fund users
+        fundingToken.mint(user1, 1000 * 1e18);
+        fundingToken.mint(user2, 1000 * 1e18);
+        fundingToken.mint(address(this), 1000 * 1e18);
+
+        // Label addresses for better debugging
+        vm.label(address(launchpad), "launchpad");
+        vm.label(address(adapter), "adapter");
+        vm.label(address(fundingToken), "fundingToken");
+        vm.label(owner, "owner");
+        vm.label(cron, "cron");
+        vm.label(user1, "user1");
+        vm.label(user2, "user2");
+    }
+
+    function test_initialization() public {
+        assertEq(address(launchpad.fundingToken()), address(fundingToken));
+        assertEq(address(launchpad.adapter()), address(adapter));
+        assertEq(launchpad.owner(), owner);
+        assertEq(launchpad.cron(), cron);
+        assertEq(launchpad.launchTick(), -206_200);
+        assertEq(launchpad.graduationTick(), -180_000);
+        assertEq(launchpad.upperMaxTick(), 886_000);
+        assertEq(launchpad.getTotalTokens(), 0);
+    }
+
+    function test_createAndBuy_noAmount() public {
+        ITokenLaunchpad.CreateParams memory params = ITokenLaunchpad.CreateParams({
+            salt: keccak256("test-salt"),
+            name: "Test Token",
+            symbol: "TEST",
+            metadata: "ipfs://test"
+        });
+
+        vm.prank(user1);
+        (address token, uint256 received, uint256 swapped, uint256 tokenId) =
+            launchpad.createAndBuy(params, address(0), 0);
+
+        // Verify token was created
+        assertTrue(token != address(0));
+        SomeToken tokenContract = SomeToken(token);
+        assertEq(tokenContract.name(), "Test Token");
+        assertEq(tokenContract.symbol(), "TEST");
+
+        // Verify NFT was minted
+        assertEq(launchpad.ownerOf(tokenId), user1);
+        assertEq(launchpad.getTotalTokens(), 1);
+
+        // Verify token was added to tokens array
+        assertEq(address(launchpad.tokens(0)), token);
+        assertEq(launchpad.tokenToNftId(tokenContract), tokenId);
+
+        // Verify swap occurred (1 ether for registration)
+        assertEq(swapped, adapter.mockSwapAmountOut());
+        assertEq(received, 0); // No additional purchase
+
+        // Verify token was approved for adapter
+        assertEq(tokenContract.allowance(address(launchpad), address(adapter)), type(uint256).max);
+    }
+
+    function test_createAndBuy_withAmount() public {
+        ITokenLaunchpad.CreateParams memory params = ITokenLaunchpad.CreateParams({
+            salt: keccak256("test-salt-2"),
+            name: "Test Token 2",
+            symbol: "TEST2",
+            metadata: "ipfs://test2"
+        });
+
+        uint256 buyAmount = 100 * 1e18;
+
+        vm.startPrank(user1);
+        fundingToken.approve(address(launchpad), buyAmount);
+
+        (address token, uint256 received, uint256 swapped, uint256 tokenId) =
+            launchpad.createAndBuy(params, address(0), buyAmount);
+        vm.stopPrank();
+
+        // Verify token was created
+        assertTrue(token != address(0));
+        SomeToken tokenContract = SomeToken(token);
+        assertEq(tokenContract.name(), "Test Token 2");
+        assertEq(tokenContract.symbol(), "TEST2");
+
+        // Verify NFT was minted
+        assertEq(launchpad.ownerOf(tokenId), user1);
+        assertEq(launchpad.getTotalTokens(), 1);
+
+        // Verify user received tokens from purchase
+        assertEq(received, adapter.mockSwapAmountOut());
+        assertEq(swapped, adapter.mockSwapAmountOut()); // Registration swap
+
+        // Verify funding token was transferred
+        assertEq(fundingToken.balanceOf(user1), 1000 * 1e18 - buyAmount);
+        assertEq(fundingToken.balanceOf(address(launchpad)), buyAmount);
+    }
+
+    function test_createAndBuy_expectedAddress() public {
+        ITokenLaunchpad.CreateParams memory params = ITokenLaunchpad.CreateParams({
+            salt: keccak256("test-salt-3"),
+            name: "Test Token 3",
+            symbol: "TEST3",
+            metadata: "ipfs://test3"
+        });
+
+        // Calculate expected address
+        bytes32 salt = keccak256(abi.encode(params.salt, user1, params.name, params.symbol));
+        address expectedToken = address(uint160(uint256(keccak256(abi.encodePacked(
+            bytes1(0xff),
+            address(launchpad),
+            salt,
+            keccak256(abi.encodePacked(
+                type(SomeToken).creationCode,
+                abi.encode(params.name, params.symbol)
+            ))
+        )))));
+
+        vm.prank(user1);
+        (address token, , , ) = launchpad.createAndBuy(params, expectedToken, 0);
+
+        assertEq(token, expectedToken);
+    }
+
+    function test_createAndBuy_invalidExpectedAddress() public {
+        ITokenLaunchpad.CreateParams memory params = ITokenLaunchpad.CreateParams({
+            salt: keccak256("test-salt-4"),
+            name: "Test Token 4",
+            symbol: "TEST4",
+            metadata: "ipfs://test4"
+        });
+
+        address wrongExpected = makeAddr("wrong");
+
+        vm.prank(user1);
+        vm.expectRevert("Invalid token address");
+        launchpad.createAndBuy(params, wrongExpected, 0);
+    }
+
+    function test_claimFees() public {
+        // First create a token
+        ITokenLaunchpad.CreateParams memory params = ITokenLaunchpad.CreateParams({
+            salt: keccak256("test-salt-5"),
+            name: "Test Token 5",
+            symbol: "TEST5",
+            metadata: "ipfs://test5"
+        });
+
+        vm.prank(user1);
+        (address token, , , uint256 tokenId) = launchpad.createAndBuy(params, address(0), 0);
+
+        // Set up mock fees
+        adapter.setMockFees(50 * 1e18, 100 * 1e18);
+
+        // Fund the adapter with tokens to distribute
+        fundingToken.mint(address(adapter), 200 * 1e18);
+        SomeToken(token).mint(address(adapter), 200 * 1e18);
+
+        // Claim fees
+        vm.expectEmit(true, true, true, true);
+        emit ITokenLaunchpad.FeeClaimed(IERC20(token), 50 * 1e18, 100 * 1e18);
+
+        launchpad.claimFees(IERC20(token));
+
+        // Verify fees were claimed
+        (uint256 claimed0, uint256 claimed1) = launchpad.claimedFees(IERC20(token));
+        assertEq(claimed0, 50 * 1e18);
+        assertEq(claimed1, 100 * 1e18);
+    }
+
+    function test_setLaunchTicks_onlyCronOrOwner() public {
+        // Test cron can set ticks
+        vm.prank(cron);
+        launchpad.setLaunchTicks(-100, -50, 100);
+
+        assertEq(launchpad.launchTick(), -100);
+        assertEq(launchpad.graduationTick(), -50);
+        assertEq(launchpad.upperMaxTick(), 100);
+
+        // Test owner can set ticks
+        vm.prank(owner);
+        launchpad.setLaunchTicks(-200, -100, 200);
+
+        assertEq(launchpad.launchTick(), -200);
+        assertEq(launchpad.graduationTick(), -100);
+        assertEq(launchpad.upperMaxTick(), 200);
+
+        // Test random user cannot set ticks
+        vm.prank(user1);
+        vm.expectRevert("!cron");
+        launchpad.setLaunchTicks(-300, -150, 300);
+    }
+
+    function test_setCron_onlyOwner() public {
+        address newCron = makeAddr("newCron");
+
+        // Test owner can set cron
+        vm.expectEmit(true, true, true, true);
+        emit ITokenLaunchpad.CronUpdated(newCron);
+
+        vm.prank(owner);
+        launchpad.setCron(newCron);
+
+        assertEq(launchpad.cron(), newCron);
+
+        // Test non-owner cannot set cron
+        vm.prank(user1);
+        vm.expectRevert();
+        launchpad.setCron(makeAddr("anotherCron"));
+    }
+
+    function test_multipleTokenCreations() public {
+        // Create multiple tokens
+        for (uint256 i = 0; i < 3; i++) {
+            ITokenLaunchpad.CreateParams memory params = ITokenLaunchpad.CreateParams({
+                salt: keccak256(abi.encode("test-salt", i)),
+                name: string(abi.encodePacked("Test Token ", i)),
+                symbol: string(abi.encodePacked("TEST", i)),
+                metadata: string(abi.encodePacked("ipfs://test", i))
+            });
+
+            vm.prank(user1);
+            (address token, , , uint256 tokenId) = launchpad.createAndBuy(params, address(0), 0);
+
+            assertEq(launchpad.ownerOf(tokenId), user1);
+            assertEq(launchpad.tokenToNftId(IERC20(token)), tokenId);
+        }
+
+        assertEq(launchpad.getTotalTokens(), 3);
+    }
+
+    function test_refundTokens() public {
+        ITokenLaunchpad.CreateParams memory params = ITokenLaunchpad.CreateParams({
+            salt: keccak256("test-salt-refund"),
+            name: "Refund Token",
+            symbol: "REFUND",
+            metadata: "ipfs://refund"
+        });
+
+        uint256 initialBalance = fundingToken.balanceOf(user1);
+
+        vm.startPrank(user1);
+        fundingToken.approve(address(launchpad), 100 * 1e18);
+        launchpad.createAndBuy(params, address(0), 100 * 1e18);
+        vm.stopPrank();
+
+        // The refund should happen automatically in createAndBuy
+        // User should have their original balance minus what they spent
+        assertEq(fundingToken.balanceOf(user1), initialBalance - 100 * 1e18);
+    }
+
+    function test_erc721_functionality() public {
+        // Create a token to get an NFT
+        ITokenLaunchpad.CreateParams memory params = ITokenLaunchpad.CreateParams({
+            salt: keccak256("test-salt-nft"),
+            name: "NFT Token",
+            symbol: "NFT",
+            metadata: "ipfs://nft"
+        });
+
+        vm.prank(user1);
+        (address token, , , uint256 tokenId) = launchpad.createAndBuy(params, address(0), 0);
+
+        // Test ERC721 functionality
+        assertEq(launchpad.ownerOf(tokenId), user1);
+        assertEq(launchpad.balanceOf(user1), 1);
+        assertEq(launchpad.tokenOfOwnerByIndex(user1, 0), tokenId);
+        assertEq(launchpad.tokenByIndex(0), tokenId);
+        assertEq(launchpad.totalSupply(), 1);
+
+        // Test transfer
+        vm.prank(user1);
+        launchpad.transferFrom(user1, user2, tokenId);
+
+        assertEq(launchpad.ownerOf(tokenId), user2);
+        assertEq(launchpad.balanceOf(user1), 0);
+        assertEq(launchpad.balanceOf(user2), 1);
+    }
+
+    function test_launchTicksUpdated_event() public {
+        vm.expectEmit(true, true, true, true);
+        emit ITokenLaunchpad.LaunchTicksUpdated(-100, -50, 100);
+
+        vm.prank(cron);
+        launchpad.setLaunchTicks(-100, -50, 100);
+    }
+
+    function test_tokenLaunched_event() public {
+        ITokenLaunchpad.CreateParams memory params = ITokenLaunchpad.CreateParams({
+            salt: keccak256("test-salt-event"),
+            name: "Event Token",
+            symbol: "EVENT",
+            metadata: "ipfs://event"
+        });
+
+        vm.expectEmit(true, true, true, true);
+        emit ITokenLaunchpad.TokenLaunched(
+            IERC20(address(0)), // Will be set to actual token address
+            address(adapter),
+            address(0), // Will be set to actual pool address
+            params
+        );
+
+        vm.prank(user1);
+        launchpad.createAndBuy(params, address(0), 0);
+    }
+
+    function test_fuzz_createAndBuy(uint256 amount) public {
+        // Bound amount to reasonable values
+        amount = bound(amount, 0, 1000 * 1e18);
+
+        ITokenLaunchpad.CreateParams memory params = ITokenLaunchpad.CreateParams({
+            salt: keccak256(abi.encode("fuzz-salt", amount)),
+            name: string(abi.encodePacked("Fuzz Token ", amount)),
+            symbol: string(abi.encodePacked("FUZZ", amount)),
+            metadata: string(abi.encodePacked("ipfs://fuzz", amount))
+        });
+
+        if (amount > 0) {
+            fundingToken.mint(user1, amount);
+            vm.startPrank(user1);
+            fundingToken.approve(address(launchpad), amount);
+            launchpad.createAndBuy(params, address(0), amount);
+            vm.stopPrank();
+        } else {
+            vm.prank(user1);
+            launchpad.createAndBuy(params, address(0), amount);
+        }
+
+        // Verify token was created regardless of amount
+        assertTrue(launchpad.getTotalTokens() > 0);
+    }
+
+    function test_receive_ether() public {
+        // Test that contract can receive ether
+        (bool success,) = address(launchpad).call{value: 1 ether}("");
+        assertTrue(success);
+        assertEq(address(launchpad).balance, 1 ether);
+    }
+}
